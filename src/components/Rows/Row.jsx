3a2bb0d6aa1e19dd643117bf37ed335f3630f816
akanksha <akanksharai9022@gmail.com>
2020-08-19 14:32:35 +0530

    updated UI

--- src/components/Rows/Row.jsx
+++ src/components/Rows/Row.jsx
@@ -8,10 +8,12 @@ const Row =props=>{
       <div>
       <div className="row-container px-2 ">
         <h4 className="heading">Suggested Topic</h4>
+        <a href="https://reactjs.org/docs/getting-started.html" className="btn btn-outline-dark top-button">More</a>
         <Cards/>
       </div>
       <div className="row-container px-2 ">
        <h4 className="heading">Modules</h4>
+       <a href="https://reactjs.org/docs/getting-started.html" className="btn btn-outline-dark top-button">More</a>
         <Pcard/>
       </div>
       </div>
